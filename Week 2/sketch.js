26c2db3598538795a86b7fd3e4a1628d3daab0fd
Jackyvk0 <jackyklooster4@gmail.com>
2026-09-10 14:06:41 +0200

    klein begin week 2: Dynamische Landschap

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
 function draw() {
-  background(220);
+  background('lightblue');
+  //De Weg
+  fill(150);
+  rect(0,500,800,605)
+  stroke('grey');
+  strokeWeight(5);
+  line(60,40,); 
+
+  stroke('whte');
+  strokeWeight(7)
+  line(5,550,70,550);
+  line(260,550,320,550);
+  line(500,550,560,550)
+  line(740,550,800,550)
+
+
 }
