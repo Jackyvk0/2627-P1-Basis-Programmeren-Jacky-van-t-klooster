e098fada842711e3aa4ba359a48084a160f585d5
Jackyvk0 <jackyklooster4@gmail.com>
2026-09-21 12:29:34 +0200

    Verder met weekopdracht 3

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,3 +1,56 @@
+//SpelersKleur
+let playerRood = 1;
+let playerBlauw = 2;
+
+//Bovenste rij
+let squareTopLeftX = 180;
+let squareTopLeftY = 180;
+let squareTopLeftS = 90;
+let squareTopLeftR = 10;
+
+let squareTopMidX = 280;
+let squareTopMidY = 180;
+let squareTopMidS = 90;
+let squareTopMidR = 10;
+
+let squareTopRightX = 380;
+let squareTopRightY = 180;
+let squareTopRightS = 90;
+let squareTopRightR = 10;
+
+//Middelste rij
+let squareMidLeftX = 180;
+let squareMidLeftY = 280;
+let squareMidLeftS = 90;
+let squareMidLeftR = 10;
+
+let squareMidMidX = 280;
+let squareMidMidY = 280;
+let squareMidMidS = 90;
+let squareMidMidR = 10;
+
+let squareMidRightX = 380;
+let squareMidRightY = 280;
+let squareMidRightS = 90;
+let squareMidRightR = 10;
+
+//Onderste rij
+let squareBottomLeftX = 180;
+let squareBottomLeftY = 380;
+let squareBottomLeftS = 90;
+let squareBottomLeftR = 10;
+
+let squareBottomMidX = 280;
+let squareBottomMidY = 380;
+let squareBottomMidS = 90;
+let squareBottomMidR = 10;
+
+let squareBottomRightX = 380;
+let squareBottomRightY = 380;
+let squareBottomRightS = 90;
+let squareBottomRightR = 10;
+
+
 
 function setup() {
   createCanvas(650, 650);
@@ -5,6 +58,9 @@ function setup() {
 
 function draw() {
   background(220);
+  fill('white');
+  textSize(32);
+  strokeWeight(0);
  
   //rode en blauwe gedeeltes
   fill('red');
@@ -12,30 +68,138 @@ rect(0,0,330, 1000);
 fill('blue');
 rect(320,0,330, 1000);
  
+//tekst Boter kaas en eieren
+strokeWeight(5);
+fill(255,255,255,150);
+rect(130,50,400,80,100,);
+ fill('black');
+  textStyle(BOLDITALIC);
+   text("Boter, Kaas en Eieren", 170, 100,);
 
- //rand boter, kaas en eieren
+//Start Knop + tekst
+fill(0,0,0, 100);
+ rect(210,530,200,80,40,);
+fill(255);
+textStyle(BOLD);
+text ('Start', 280, 580);
+
+//rand boter, kaas en eieren
+strokeWeight(0);
 fill('black');
   square (160,160, 330, 20);
+  
+  
+  //3x3 knoppen hover
+  //Top Left
+  
+  if (
+    mouseX > squareTopLeftX && mouseX < squareTopLeftX + squareTopLeftS && 
+    mouseY > squareTopLeftY && mouseY < squareTopLeftY + squareTopLeftS
+  ) {
+    fill(100);
+    square (squareTopLeftX,squareTopLeftY,squareTopLeftS,squareTopLeftR);
+  } else {
+    fill (160);
+    square (squareTopLeftX,squareTopLeftY,squareTopLeftS,squareTopLeftR);
+  }
 
-  //3x3 knoppen
-  fill(160);
+  //Top Middle
+   if (
+    mouseX > squareTopMidX && mouseX < squareTopMidX + squareTopMidS && 
+    mouseY > squareTopMidY && mouseY < squareTopMidY + squareTopMidS
+  ) {
+    fill(100);
+    square (squareTopMidX,squareTopMidY,squareTopMidS,squareTopMidR);
+  } else {
+    fill (160);
+    square (squareTopMidX,squareTopMidY,squareTopMidS,squareTopMidR);
+  }
 
-  //bovenste rij
-  square (180,180,90, 10);
-  square (280,180,90, 10);
-  square (380,180,90, 10);
+  //Top Right
+  if (
+    mouseX > squareTopRightX && mouseX < squareTopRightX + squareTopRightS && 
+    mouseY > squareTopRightY && mouseY < squareTopRightY + squareTopRightS
+  ) {
+    fill(100);
+    square (squareTopRightX,squareTopRightY,squareTopRightS,squareTopRightR);
+  } else {
+    fill (160);
+    square (squareTopRightX,squareTopRightY,squareTopRightS,squareTopRightR);
+  }
+  
+//Middle Left
 
- //middelste rij
-  square (180,280,90, 10);
-  square (280,280,90, 10);
-  square (380,280,90, 10);
+ if (
+    mouseX > squareMidLeftX && mouseX < squareMidLeftX + squareMidLeftS && 
+    mouseY > squareMidLeftY && mouseY < squareMidLeftY + squareMidLeftS
+  ) {
+    fill(100);
+    square (squareMidLeftX,squareMidLeftY,squareMidLeftS,squareMidLeftR);
+  } else {
+    fill (160);
+    square (squareMidLeftX,squareMidLeftY,squareMidLeftS,squareMidLeftR);
+  }
+  
+//Middle Mid
 
-  //onderste rij
-  square (180,380,90, 10);
-  square (280,380,90, 10);
-  square (380,380,90, 10);
+ if (
+    mouseX > squareMidMidX && mouseX < squareMidMidX + squareMidMidS && 
+    mouseY > squareMidMidY && mouseY < squareMidMidY + squareMidMidS
+  ) {
+    fill(100);
+    square (squareMidMidX,squareMidMidY,squareMidMidS,squareMidMidR);
+  } else {
+    fill (160);
+    square (squareMidMidX,squareMidMidY,squareMidMidS,squareMidMidR);
+  }
+
+  //Middle Right
+  if (
+    mouseX > squareMidRightX && mouseX < squareMidRightX + squareMidRightS && 
+    mouseY > squareMidRightY && mouseY < squareMidRightY + squareMidRightS
+  ) {
+    fill(100);
+    square (squareMidRightX,squareMidRightY,squareMidMidS,squareMidRightR);
+  } else {
+    fill (160);
+    square (squareMidRightX,squareMidRightY,squareMidRightS,squareMidRightR);
+  }
+
+  //Bottom left
+  if (
+    mouseX > squareBottomLeftX && mouseX < squareBottomLeftX + squareBottomLeftS && 
+    mouseY > squareBottomLeftY && mouseY < squareBottomLeftY + squareBottomLeftS
+  ) {
+    fill(100);
+    square (squareBottomLeftX,squareBottomLeftY,squareBottomLeftS,squareBottomLeftR);
+  } else {
+    fill (160);
+    square (squareBottomLeftX,squareBottomLeftY,squareBottomLeftS,squareBottomLeftR);
+  }
+
+  //Bottom right
+  if (
+    mouseX > squareBottomMidX && mouseX < squareBottomMidX + squareBottomMidS && 
+    mouseY > squareBottomMidY && mouseY < squareBottomMidY + squareBottomMidS
+  ) {
+    fill(100);
+    square (squareBottomMidX,squareBottomMidY,squareBottomMidS,squareBottomMidR);
+  } else {
+    fill (160);
+    square (squareBottomMidX,squareBottomMidY,squareBottomMidS,squareBottomMidR);
+  }
+
+   if (
+    mouseX > squareBottomRightX && mouseX < squareBottomRightX + squareBottomRightS && 
+    mouseY > squareBottomRightY && mouseY < squareBottomRightY + squareBottomRightS
+  ) {
+    fill(100);
+    square (squareBottomRightX,squareBottomRightY,squareBottomRightS,squareBottomRightR);
+  } else {
+    fill (160);
+    square (squareBottomRightX,squareBottomRightY,squareBottomRightS,squareBottomRightR);
+  }
 
 
-
-
-}
+  }
+ 
