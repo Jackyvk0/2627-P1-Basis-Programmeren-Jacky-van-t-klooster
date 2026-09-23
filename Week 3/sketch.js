69fe756e4aadfb95be662768c901b3e808e6a0aa
Jackyvk0 <jackyklooster4@gmail.com>
2026-09-23 21:58:53 +0200

    Restarten en winnen werkt

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,15 +1,11 @@
+//startknop en spelstatus
+let spelGestart = false;
+let winnaar = 0; // 0 = geen, 1 = Rood wint, 2 = Blauw wint, 3 = Gelijkspel
 
-// let currentplayer = random(1,2);
-// text (currentplayer, 200, 60);
-
-// if (player == 1) {
-//   text ('Reds turn', 200, 500);
-// } else if (player == 2 ) {
-//  text('Blues turn', 200, 500);
-// }
-
-
-
+//status van de vakjes (0 = leeg, 1 = rood, 2 = blauw)
+let stateTopLeft = 0, stateTopMid = 0, stateTopRight = 0;
+let stateMidLeft = 0, stateMidMid = 0, stateMidRight = 0;
+let stateBottomLeft = 0, stateBottomMid = 0, stateBottomRight = 0;
 
 //Bovenste rij kleur
 let colorSquareTopLeft = 'darkgrey'
@@ -114,12 +110,23 @@ function draw() {
   rect(320, 0, 330, 1000);
 
   //tekst Boter kaas en eieren
-  strokeWeight(5);
-  fill(50, 50, 50, 100);
-  rect(130, 50, 400, 80, 100,);
-  fill(255);
-  textStyle(BOLDITALIC);
-  text("Boter, Kaas en Eieren", 170, 100,);
+  if (spelGestart == false) {
+    strokeWeight(5);
+    fill(50, 50, 50, 100);
+    rect(130, 50, 400, 80, 100);
+    fill(255);
+    textStyle(BOLDITALIC);
+    text("Boter, Kaas en Eieren", 170, 100);
+
+    // Start Knop
+    strokeWeight(3);
+    fill('green');
+    rect(225, 550, 200, 60, 20); // de knop zelf
+    fill('white');
+    strokeWeight(0);
+    textSize(30);
+    text("START", 280, 590); // tekst in de knop
+  }
 
   //rand boter, kaas en eieren
   strokeWeight(0);
@@ -247,12 +254,38 @@ function draw() {
     square(squareBottomRightX, squareBottomRightY, squareBottomRightS, squareBottomRightR);
   }
 
-}
+  //winnaar en restart knop
+  if (winnaar > 0) {
+    strokeWeight(5);
+    fill(50, 50, 50, 200);
+    rect(100, 50, 450, 80, 20);
+    fill(255);
+    textSize(28);
+    textStyle(BOLD);
 
+    if (winnaar == 1) {
+      text("Rood heeft gewonnen!", 170, 100);
+    } else if (winnaar == 2) {
+      text("Blauw heeft gewonnen!", 160, 100);
+    } else if (winnaar == 3) {
+      text("Gelijkspel!", 250, 100);
+    }
+
+    // Restart Knop
+    strokeWeight(3);
+    fill('orange');
+    rect(225, 550, 200, 60, 20);
+    fill('white');
+    strokeWeight(0);
+    textSize(26);
+    text("Restart", 265, 590);
+  }
+}
 
 
 function changeTurn()
 {
+    checkWinnaar();
     player = player + 1;
     if (player > 2) {
       player = 1
@@ -260,151 +293,213 @@ function changeTurn()
 }
 
 
+function checkWinnaar() {
+  //alle 8 winnende plekken
+  
+  // Rood wint (player 1)
+  if (
+    (stateTopLeft == 1 && stateTopMid == 1 && stateTopRight == 1) ||
+    (stateMidLeft == 1 && stateMidMid == 1 && stateMidRight == 1) ||
+    (stateBottomLeft == 1 && stateBottomMid == 1 && stateBottomRight == 1) ||
+    (stateTopLeft == 1 && stateMidLeft == 1 && stateBottomLeft == 1) ||
+    (stateTopMid == 1 && stateMidMid == 1 && stateBottomMid == 1) ||
+    (stateTopRight == 1 && stateMidRight == 1 && stateBottomRight == 1) ||
+    (stateTopLeft == 1 && stateMidMid == 1 && stateBottomRight == 1) ||
+    (stateTopRight == 1 && stateMidMid == 1 && stateBottomLeft == 1)
+  ) {
+    winnaar = 1;
+  }
+  
+  // Blauw wint (player 2)
+  else if (
+    (stateTopLeft == 2 && stateTopMid == 2 && stateTopRight == 2) ||
+    (stateMidLeft == 2 && stateMidMid == 2 && stateMidRight == 2) ||
+    (stateBottomLeft == 2 && stateBottomMid == 2 && stateBottomRight == 2) ||
+    (stateTopLeft == 2 && stateMidLeft == 2 && stateBottomLeft == 2) ||
+    (stateTopMid == 2 && stateMidMid == 2 && stateBottomMid == 2) ||
+    (stateTopRight == 2 && stateMidRight == 2 && stateBottomRight == 2) ||
+    (stateTopLeft == 2 && stateMidMid == 2 && stateBottomRight == 2) ||
+    (stateTopRight == 2 && stateMidMid == 2 && stateBottomLeft == 2)
+  ) {
+    winnaar = 2;
+  }
+  
+  // Gelijkspel
+  else if (
+    stateTopLeft != 0 && stateTopMid != 0 && stateTopRight != 0 &&
+    stateMidLeft != 0 && stateMidMid != 0 && stateMidRight != 0 &&
+    stateBottomLeft != 0 && stateBottomMid != 0 && stateBottomRight != 0
+  ) {
+    winnaar = 3;
+  }
+}
+
+
+function restartSpel() {
+  //terugzetten naar beginstand
+  player = 1;
+  winnaar = 0;
+
+  stateTopLeft = 0; stateTopMid = 0; stateTopRight = 0;
+  stateMidLeft = 0; stateMidMid = 0; stateMidRight = 0;
+  stateBottomLeft = 0; stateBottomMid = 0; stateBottomRight = 0;
+
+  colorSquareTopLeft = 'darkgrey'; hovercolorSquareTopLeft = 'grey';
+  colorSquareTopMid = 'darkgrey'; hovercolorSquareTopMid = 'grey';
+  colorSquareTopRight = 'darkgrey'; hovercolorSquareTopRight = 'grey';
+
+  colorSquareMidLeft = 'darkgrey'; hovercolorSquareMidLeft = 'grey';
+  colorSquareMidMid = 'darkgrey'; hovercolorSquareMidMid = 'grey';
+  colorSquareMidRight = 'darkgrey'; hovercolorSquareMidRight = 'grey';
+
+  colorSquareBottomLeft = 'darkgrey'; hovercolorSquareBottomLeft = 'grey';
+  colorSquareBottomMid = 'darkgrey'; hovercolorSquareBottomMid = 'grey';
+  colorSquareBottomRight = 'darkgrey'; hovercolorSquareBottomRight = 'grey';
+}
 
 
 function mouseClicked() {
-  //Top left kleur click
-  if (mouseX > squareTopLeftX && mouseX < squareTopLeftX + squareTopLeftS &&
-    mouseY > squareTopLeftY && mouseY < squareTopLeftY + squareTopLeftS
-  ) {
 
-    if (player == 1) {
-      colorSquareTopLeft = 'red'
-      hovercolorSquareTopLeft = 'red'
-    } else if (player == 2) {
-      colorSquareTopLeft = 'blue'
-      hovercolorSquareTopLeft = 'blue'
+  // Kijken of er op start wordt geklikt
+  if (spelGestart == false) {
+    if (mouseX > 225 && mouseX < 425 && mouseY > 550 && mouseY < 610) {
+      spelGestart = true;
     }
-
-    changeTurn();
-
-  }
-  // Top mid kleur click
-  if (mouseX > squareTopMidX && mouseX < squareTopMidX + squareTopMidS &&
-    mouseY > squareTopMidY && mouseY < squareTopMidY + squareTopMidS
-  ) {
-
-    if (player == 1) {
-      colorSquareTopMid = 'red'
-      hovercolorSquareTopMid = 'red'
-    } else if (player == 2) {
-      colorSquareTopMid = 'blue'
-      hovercolorSquareTopMid = 'blue'
-    }
-
-    changeTurn();
   }
 
-  //Top Right kleur click
-  if (mouseX > squareTopRightX && mouseX < squareTopRightX + squareTopRightS &&
-    mouseY > squareTopRightY && mouseY < squareTopRightY + squareTopRightS
-  ) {
-
-     if (player == 1) {
-      colorSquareTopRight = 'red'
-      hovercolorSquareTopRight = 'red'
-    } else if (player == 2) {
-      colorSquareTopRight= 'blue'
-      hovercolorSquareTopRight = 'blue'
+  // Kijken of er op restart wordt geklikt
+  if (winnaar > 0) {
+    if (mouseX > 225 && mouseX < 425 && mouseY > 550 && mouseY < 610) {
+      restartSpel();
     }
-
-    changeTurn();
   }
 
-  //Mid left kleur click
-  if (mouseX > squareMidLeftX && mouseX < squareMidLeftX + squareMidLeftS &&
-    mouseY > squareMidLeftY && mouseY < squareMidLeftY + squareMidLeftS
-  ) {
-    
-     if (player == 1) {
-      colorSquareMidLeft = 'red'
-      hovercolorSquareMidLeft = 'red'
-    } else if (player == 2) {
-      colorSquareMidLeft= 'blue'
-      hovercolorSquareMidLeft = 'blue'
+  // als het spel is gestart en er nog geen winnaar mag je vakjes klikken
+  if (spelGestart == true && winnaar == 0) {
+
+    // Top left kleur click
+    if (mouseX > squareTopLeftX && mouseX < squareTopLeftX + squareTopLeftS &&
+      mouseY > squareTopLeftY && mouseY < squareTopLeftY + squareTopLeftS
+    ) {
+      if (stateTopLeft == 0) { // Alleen als vakje nog leeg is
+        if (player == 1) {
+          colorSquareTopLeft = 'red'; hovercolorSquareTopLeft = 'red'; stateTopLeft = 1;
+        } else if (player == 2) {
+          colorSquareTopLeft = 'blue'; hovercolorSquareTopLeft = 'blue'; stateTopLeft = 2;
+        }
+        changeTurn();
+      }
+    }
+    // Top mid kleur click
+    if (mouseX > squareTopMidX && mouseX < squareTopMidX + squareTopMidS &&
+      mouseY > squareTopMidY && mouseY < squareTopMidY + squareTopMidS
+    ) {
+      if (stateTopMid == 0) {
+        if (player == 1) {
+          colorSquareTopMid = 'red'; hovercolorSquareTopMid = 'red'; stateTopMid = 1;
+        } else if (player == 2) {
+          colorSquareTopMid = 'blue'; hovercolorSquareTopMid = 'blue'; stateTopMid = 2;
+        }
+        changeTurn();
+      }
     }
 
-    changeTurn();
-  }
-  // Mid mid kleur click
-  if (mouseX > squareMidMidX && mouseX < squareMidMidX + squareMidMidS &&
-    mouseY > squareMidMidY && mouseY < squareMidMidY + squareMidMidS
-  ) {
-
-    colorSquareMidMid = 'red'
-    hovercolorSquareMidMid = 'red'
-
-     if (player == 1) {
-      colorSquareMidMid = 'red'
-      hovercolorSquareMidMid = 'red'
-    } else if (player == 2) {
-      colorSquareMidMid= 'blue'
-      hovercolorSquareMidMid = 'blue'
+    //Top Right kleur click
+    if (mouseX > squareTopRightX && mouseX < squareTopRightX + squareTopRightS &&
+      mouseY > squareTopRightY && mouseY < squareTopRightY + squareTopRightS
+    ) {
+      if (stateTopRight == 0) {
+        if (player == 1) {
+          colorSquareTopRight = 'red'; hovercolorSquareTopRight = 'red'; stateTopRight = 1;
+        } else if (player == 2) {
+          colorSquareTopRight = 'blue'; hovercolorSquareTopRight = 'blue'; stateTopRight = 2;
+        }
+        changeTurn();
+      }
     }
 
-    changeTurn();
-  }
-
-  //Mid Right kleur click
-  if (mouseX > squareMidRightX && mouseX < squareMidRightX + squareMidRightS &&
-    mouseY > squareMidRightY && mouseY < squareMidRightY + squareMidRightS
-  ) {
-
-     if (player == 1) {
-      colorSquareMidRight = 'red'
-      hovercolorSquareMidRight = 'red'
-    } else if (player == 2) {
-      colorSquareMidRight= 'blue'
-      hovercolorSquareMidRight = 'blue'
+    //Mid left kleur click
+    if (mouseX > squareMidLeftX && mouseX < squareMidLeftX + squareMidLeftS &&
+      mouseY > squareMidLeftY && mouseY < squareMidLeftY + squareMidLeftS
+    ) {
+      if (stateMidLeft == 0) {
+        if (player == 1) {
+          colorSquareMidLeft = 'red'; hovercolorSquareMidLeft = 'red'; stateMidLeft = 1;
+        } else if (player == 2) {
+          colorSquareMidLeft = 'blue'; hovercolorSquareMidLeft = 'blue'; stateMidLeft = 2;
+        }
+        changeTurn();
+      }
+    }
+    // Mid mid kleur click
+    if (mouseX > squareMidMidX && mouseX < squareMidMidX + squareMidMidS &&
+      mouseY > squareMidMidY && mouseY < squareMidMidY + squareMidMidS
+    ) {
+      if (stateMidMid == 0) {
+        if (player == 1) {
+          colorSquareMidMid = 'red'; hovercolorSquareMidMid = 'red'; stateMidMid = 1;
+        } else if (player == 2) {
+          colorSquareMidMid = 'blue'; hovercolorSquareMidMid = 'blue'; stateMidMid = 2;
+        }
+        changeTurn();
+      }
     }
 
-    changeTurn();
-
-  }
-
-  //Bottom left kleur click
-  if (mouseX > squareBottomLeftX && mouseX < squareBottomLeftX + squareBottomLeftS &&
-    mouseY > squareBottomLeftY && mouseY < squareBottomLeftY + squareBottomLeftS
-  ) {
-     if (player == 1) {
-      colorSquareBottomLeft = 'red'
-      hovercolorSquareBottomLeft = 'red'
-    } else if (player == 2) {
-      colorSquareBottomLeft= 'blue'
-      hovercolorSquareBottomLeft = 'blue'
+    //Mid Right kleur click
+    if (mouseX > squareMidRightX && mouseX < squareMidRightX + squareMidRightS &&
+      mouseY > squareMidRightY && mouseY < squareMidRightY + squareMidRightS
+    ) {
+      if (stateMidRight == 0) {
+        if (player == 1) {
+          colorSquareMidRight = 'red'; hovercolorSquareMidRight = 'red'; stateMidRight = 1;
+        } else if (player == 2) {
+          colorSquareMidRight = 'blue'; hovercolorSquareMidRight = 'blue'; stateMidRight = 2;
+        }
+        changeTurn();
+      }
     }
 
-    changeTurn();
-  }
-  //Bottom mid kleur click
-  if (mouseX > squareBottomMidX && mouseX < squareBottomMidX + squareBottomMidS &&
-    mouseY > squareBottomMidY && mouseY < squareBottomMidY + squareBottomMidS
-  ) {
-         if (player == 1) {
-      colorSquareBottomMid = 'red'
-      hovercolorSquareBottomMid = 'red'
-    } else if (player == 2) {
-      colorSquareBottomMid= 'blue'
-      hovercolorSquareBottomMid = 'blue'
+    //Bottom left kleur click
+    if (mouseX > squareBottomLeftX && mouseX < squareBottomLeftX + squareBottomLeftS &&
+      mouseY > squareBottomLeftY && mouseY < squareBottomLeftY + squareBottomLeftS
+    ) {
+      if (stateBottomLeft == 0) {
+        if (player == 1) {
+          colorSquareBottomLeft = 'red'; hovercolorSquareBottomLeft = 'red'; stateBottomLeft = 1;
+        } else if (player == 2) {
+          colorSquareBottomLeft = 'blue'; hovercolorSquareBottomLeft = 'blue'; stateBottomLeft = 2;
+        }
+        changeTurn();
+      }
+    }
+    //Bottom mid kleur click
+    if (mouseX > squareBottomMidX && mouseX < squareBottomMidX + squareBottomMidS &&
+      mouseY > squareBottomMidY && mouseY < squareBottomMidY + squareBottomMidS
+    ) {
+      if (stateBottomMid == 0) {
+        if (player == 1) {
+          colorSquareBottomMid = 'red'; hovercolorSquareBottomMid = 'red'; stateBottomMid = 1;
+        } else if (player == 2) {
+          colorSquareBottomMid = 'blue'; hovercolorSquareBottomMid = 'blue'; stateBottomMid = 2;
+        }
+        changeTurn();
+      }
     }
 
-    changeTurn();
-  }
-
-  //Bottom Right kleur click
-  if (mouseX > squareBottomRightX && mouseX < squareBottomRightX + squareBottomRightS &&
-    mouseY > squareBottomRightY && mouseY < squareBottomRightY + squareBottomRightS
-  ) {
-         if (player == 1) {
-      colorSquareBottomRight = 'red'
-      hovercolorSquareBottomRight = 'red'
-    } else if (player == 2) {
-      colorSquareBottomRight= 'blue'
-      hovercolorSquareBottomRight = 'blue'
+    //Bottom Right kleur click
+    if (mouseX > squareBottomRightX && mouseX < squareBottomRightX + squareBottomRightS &&
+      mouseY > squareBottomRightY && mouseY < squareBottomRightY + squareBottomRightS
+    ) {
+      if (stateBottomRight == 0) {
+        if (player == 1) {
+          colorSquareBottomRight = 'red'; hovercolorSquareBottomRight = 'red'; stateBottomRight = 1;
+        } else if (player == 2) {
+          colorSquareBottomRight = 'blue'; hovercolorSquareBottomRight = 'blue'; stateBottomRight = 2;
+        }
+        changeTurn();
+      }
     }
 
-    changeTurn();
   }
 
 }
-
